8191752fc5fad384cecefe721c94e64a6a1a4bac
Devipriya7811 <devipriyas7811@gmail.com>
2024-02-20 22:52:39 +0530

    url update

--- src/url.js
+++ src/url.js
@@ -1,3 +1,3 @@
-// export const URL="https://foal-cuff-links.cyclic.app"
-export const URL="http://localhost:5000"
+export const URL="https://foal-cuff-links.cyclic.app"
+// export const URL="http://localhost:5000"
 export const IF="https://foal-cuff-links.cyclic.app/images" 
